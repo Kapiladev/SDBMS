b6c4fa32035da0ae3c91e83d54238def0df37618
KAPILADEV <110283672+Kapiladev@users.noreply.github.com>
2023-02-22 16:52:43 +0530

    Add files via upload

---
+++ Calculater.java
@@ -0,0 +1,8 @@
+package sdbms;
+
+public interface Calculater {
+	void add(int x, int y);
+	void sub(int x, int y);
+	void mul(int x, int y);
+	void div(int x, int y);
+}
---
+++ CalculaterImpl.java
@@ -0,0 +1,33 @@
+package sdbms;
+
+public class CalculaterImpl implements Calculater {
+
+	
+	
+	
+	
+	
+	public void add(int x, int y) {
+		
+		
+	}
+
+	@Override
+	public void sub(int x, int y) {
+		
+		
+	}
+
+	@Override
+	public void mul(int x, int y) {
+		
+		
+	}
+
+	@Override
+	public void div(int x, int y) {
+		
+		
+	}
+
+}
