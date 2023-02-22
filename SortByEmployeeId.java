494c60da06cdfb1de80b2b96c7b04a62855a0f64
KAPILADEV <110283672+Kapiladev@users.noreply.github.com>
2023-02-22 17:01:20 +0530

    Delete SortByEmployeeId.java

--- SortByEmployeeId.java
+++
@@ -1,9 +0,0 @@
-package sdbms;
-
-import java.util.Comparator;
-
-public class SortByEmployeeId implements Comparator<Employee>{ 
-    public int compare(Employee x,Employee y) {
-	return x.id-y.id;
-}
-}
